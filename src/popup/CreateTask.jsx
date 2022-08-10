67fa05dfc60a153fd9ad7268fab5776734b77a44
Seyi0 <makanjuolaseyi560@gmail.com>
2022-08-10 11:05:32 +0100

    first commit

---
+++ src/popup/CreateTask.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
+const CreateTask = ({modal, toggle, save}) => {
+    const [taskName, setTaskName] = useState('');
+    const [details, setDetails] = useState('');
+
+    const handleChange = (e) => {
+        
+        const {name, value} = e.target
+
+        if(name === "taskName"){
+            setTaskName(value)
+        }else{
+            setDetails(value)
+        }
+
+
+    }
+
+    const handleSave = (e) => {
+        e.preventDefault()
+        let taskObj = {}
+        taskObj["Name"] = taskName
+        taskObj["Details"] = details
+        save(taskObj)
+
+    }
+
+    return (
+        <Modal isOpen={modal} toggle={toggle}>
+            <ModalHeader toggle={toggle}>Create Task</ModalHeader>
+            <ModalBody>
+            
+                    <div className = "form-group">
+                        <label>Task Name</label>
+                        <input type="text" className = "form-control" value = {taskName} onChange = {handleChange} name = "taskName"/>
+                    </div>
+                    <div className = "form-group">
+                        <label>Description</label>
+                        <textarea rows = "5" className = "form-control" value = {details} onChange = {handleChange} name = "details"></textarea>
+                    </div>
+                
+            </ModalBody>
+            <ModalFooter>
+            <Button color="primary" onClick={handleSave}>Create</Button>{' '}
+            <Button color="secondary" onClick={toggle}>Cancel</Button>
+            </ModalFooter>
+      </Modal>
+    );
+};
+
+export default CreateTask;
